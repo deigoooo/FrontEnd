3c8c48f22bb86b0de3ad57f0faf103027db5157e
Diego Fernando Ramirez <99920719+deigoooo@users.noreply.github.com>
2022-06-15 19:57:03 -0300

    Se crearon los componentes necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcercadeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
